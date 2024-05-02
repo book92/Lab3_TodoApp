05d0cf0bb91ee177f266613c726eade45ae5931b
Your Name <you@example.com>
2024-05-02 22:09:24 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import firestore from '@react-native-firebase/firestore';
+import {FlatList, View, ScrollView, Text, StyleSheet} from 'react-native';
+import {Appbar, TextInput, Button} from 'react-native-paper';
+import Todo from './todo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App() {
+  const [todo, setTodo] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
+  const [todos, setTodos] = React.useState([]);
+  const ref = firestore().collection('todos');
+  async function addTodo(){
+    await ref.add({
+      title: todo,
+      complete: false,
+    });
+    setTodo('');
+  }
+  React.useEffect(() => { //real time
+    return ref.onSnapshot(querySnapshot => {
+      const list = [];
+      querySnapshot.forEach(doc => {
+        const {title, complete} = doc.data();
+        list.push({
+          id: doc.id,
+          title,
+          complete,
+        });
+      });
+      setTodos(list);
+
+      if(loading){
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading){
+    return null;
+  }
+
+  return(
+    <View style={{flex:1}}>
+      <Appbar>
+        <Appbar.Content title={'TODOs List'}/>
+      </Appbar>
+      <FlatList
+        style={{flex: 1}}
+        data={todos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Todo {...item}/>}
+      />
+      <TextInput label={'New Todo'} value={todo} onChangeText={(text) => setTodo(text)}/>
+      <Button textColor="black"  onPress={addTodo}>ADD TODO</Button>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
